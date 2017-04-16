74e4e65c1e2d6b82b320eb5da94e12e0ea1965b6
julia951 <klimuk.julia@gmail.com>
2017-04-16 11:31:41 +0300

    Commit1

---
+++ src/main/java/EventHandler.java
@@ -0,0 +1,94 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.events.WebDriverEventListener;
+
+/**
+ * Created by julia on 15.04.2017.
+ */
+public class EventHandler implements WebDriverEventListener{
+
+    public void beforeAlertAccept(WebDriver webDriver) {
+
+    }
+
+    public void afterAlertAccept(WebDriver webDriver) {
+
+    }
+
+    public void afterAlertDismiss(WebDriver webDriver) {
+
+    }
+
+    public void beforeAlertDismiss(WebDriver webDriver) {
+
+    }
+
+    public void beforeNavigateTo(String s, WebDriver webDriver) {
+
+    }
+
+    public void afterNavigateTo(String s, WebDriver webDriver) {
+
+    }
+
+    public void beforeNavigateBack(WebDriver webDriver) {
+
+    }
+
+    public void afterNavigateBack(WebDriver webDriver) {
+
+    }
+
+    public void beforeNavigateForward(WebDriver webDriver) {
+
+    }
+
+    public void afterNavigateForward(WebDriver webDriver) {
+
+    }
+
+    public void beforeNavigateRefresh(WebDriver webDriver) {
+
+    }
+
+    public void afterNavigateRefresh(WebDriver webDriver) {
+
+    }
+
+    public void beforeFindBy(By by, WebElement webElement, WebDriver webDriver) {
+System.out.println("looking for an element"+by);
+    }
+
+    public void afterFindBy(By by, WebElement webElement, WebDriver webDriver) {
+System.out.print("found element" + by);
+    }
+
+    public void beforeClickOn(WebElement webElement, WebDriver webDriver) {
+
+    }
+
+    public void afterClickOn(WebElement webElement, WebDriver webDriver) {
+
+    }
+
+    public void beforeChangeValueOf(WebElement webElement, WebDriver webDriver, CharSequence[] charSequences) {
+
+    }
+
+    public void afterChangeValueOf(WebElement webElement, WebDriver webDriver, CharSequence[] charSequences) {
+
+    }
+
+    public void beforeScript(String s, WebDriver webDriver) {
+
+    }
+
+    public void afterScript(String s, WebDriver webDriver) {
+
+    }
+
+    public void onException(Throwable throwable, WebDriver webDriver) {
+
+    }
+}
---
+++ src/main/java/HW1.java
@@ -0,0 +1,81 @@
+import com.gargoylesoftware.htmlunit.javascript.background.JavaScriptExecutor;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import javax.swing.*;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by julia on 15.04.2017.
+ */
+public class HW1 {
+    public static void main(String[] args) {
+        String property = System.getProperty("user.dir")+"/driver/chromedriver.exe";
+        System.setProperty("webdriver.chrome.driver",property);
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("http://prestashop-automation.qatestlab.com.ua/admin147ajyvk0/");
+
+        WebElement input_email = driver.findElement(By.name("email"));
+        input_email.sendKeys("webinar.test@gmail.com");
+        WebElement input_pass = driver.findElement(By.name("passwd"));
+        input_pass.sendKeys("Xcg7299bnSmMuRLp9ITw");
+        WebElement btn = driver.findElement(By.name("submitLogin"));
+        btn.click();
+        WebDriverWait wait1 = new WebDriverWait(driver,10);
+
+        //enter the catalog
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        WebElement catalog = driver.findElement(By.id("subtab-AdminCatalog"));
+        Actions builder = new Actions(driver);
+        builder.moveToElement(catalog).build().perform();
+
+        //enter the category
+        wait1.until(ExpectedConditions.presenceOfElementLocated(By.id("subtab-AdminCategories")));
+        WebElement subcatalog = driver.findElement(By.id("subtab-AdminCategories"));
+        subcatalog.click();
+
+        wait1.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[text()='Добавить категорию']")));
+        List<WebElement> arrayBeforeSort = driver.findElements(By.xpath("//i[@class='icon-caret-down']"));
+
+        WebElement btnAdd = driver.findElement(By.xpath("//div[text()='Добавить категорию']"));
+        btnAdd.click();
+
+        //enter the text to the input
+        wait1.until(ExpectedConditions.presenceOfElementLocated(By.className("copy2friendlyUrl")));
+        WebElement input = driver.findElement(By.className("copy2friendlyUrl"));
+        input.sendKeys("Тест");
+
+        //save the changes
+        WebElement btnSave = driver.findElement(By.id("category_form_submit_btn"));
+        btnSave.click();
+
+        //find the save text
+        WebElement msq = driver.findElement(By.xpath("//div[@class='alert alert-success']"));
+
+        //sort
+        WebElement sort = driver.findElement(By.xpath("//i[@class='icon-caret-down']"));
+        sort.click();
+
+        //find the list
+        wait1.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath("//td[@class='pointer']")));
+        List<WebElement> arrayAfterSort = driver.findElements(By.xpath("//i[@class='icon-caret-down']"));
+
+        if (arrayAfterSort.size()>arrayBeforeSort.size()) {
+            //find my element of list
+            System.out.print("The element is found");
+        }
+        else
+        {
+            System.out.print("The element is not found");
+        }
+        driver.quit();
+    }
+}
